1e8432d97fcbdfbd124c042f076bc92f651e13c9
Dmitry Verteyko <verteyko1990@gmail.com>
2021-02-20 23:46:09 +0300

    # login page

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const router = require('./routers/export-router');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-mongoose.connect('mongodb://localhost:27017/Chat', {
+mongoose.connect('mongodb://localhost:27017/onlineBank', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
